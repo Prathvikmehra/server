3c6729966ba5e7c075d6e3f6209e40aae0cbeb0d
prathvik mehra <prathvik.mehra.cg@gmail.com>
2026-02-17 16:15:52 +0530

    pushing in render

--- practical/index.js
+++ practical/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const users = [
     {attendence:'80',uid:108243,total_sub:14,bonus:'30',name:'dax'},
